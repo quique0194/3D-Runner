716f3683678a79ce63a13f43679ab0257256d87c
quique0194 <quique0194@gmail.com>
2014-12-05 01:16:15 -0500

    fix submodule qu3e

--- lib/genlibqu3e.sh
+++ lib/genlibqu3e.sh
@@ -1,6 +1,6 @@
-cd ../qu3e/src
+cd qu3e/src
 g++ -std=c++11 -fPIC -c `find -name '*.cpp'`
 g++ -shared -o libqu3e.so `find -name '*.o'`
 rm -rf *.o
-mv libqu3e.so ../../lib
-cd ../../lib
+mv libqu3e.so ../..
+cd ../..
