804ef17cfb2f9190e8694833656b6b7f32379a05
alex <alexander.cc.h@gmail.com>
2014-12-09 13:01:21 -0500

    sonidos agregados NENE MALO

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-g++ main.cpp Clock.cpp -std=c++11 -Llib -lGLU -lGL -lglut -lqu3e -lpthread
+g++ main.cpp Clock.cpp -std=c++11 -Llib -lGLU -lGL -lglut -lqu3e -lpthread  -lsfml-audio -lsfml-window -lsfml-system
 export LD_LIBRARY_PATH=lib
